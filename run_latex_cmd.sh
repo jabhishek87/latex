eb296fb6598f25029347944f2666a097c4e626d3
AbhishekJ <ia.malhotra@gmail.com>
2019-11-06 11:57:32 +0000

    Updated resume with Pdf resume

--- run_latex_cmd.sh
+++ run_latex_cmd.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # lualatex resume.tex
 # echo docker run --rm -i  -v "$PWD":/data abhishekk/latex
-exec docker run --rm -i  -v "$PWD":/data abhishekk/latex "$@"
+docker run --rm -i  -v "$PWD":/data abhishekk/latex "$@"
+echo "Renaming Document To Abhishek_Jaiswal"
+cp resume.pdf Abhishek_Jaiswal.pdf
